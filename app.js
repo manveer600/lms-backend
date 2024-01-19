331d51d1f1223fc37bedf677d999acddb15beb4f
manveer600 <100722147+manveer600@users.noreply.github.com>
2024-01-19 13:38:42 +0530

    added cors properly

--- app.js
+++ app.js
@@ -21,13 +21,14 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 204,
   })
 );
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
