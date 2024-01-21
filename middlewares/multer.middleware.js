58b61165e165eb65cbdc8b962e5bc12ddec521e5
manveer600 <100722147+manveer600@users.noreply.github.com>
2024-01-21 11:07:02 +0530

    updated multer

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -1,10 +1,6 @@
 import multer from 'multer';
 import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const upload = multer({
     dest: 'uploads/',
